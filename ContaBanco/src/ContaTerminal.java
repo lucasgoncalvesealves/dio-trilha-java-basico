4c1ec9b62c3f9bfe43a34bf625dc7e3bf30b21b4
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-09 23:51:53 -0300

    Update ContaTerminal.java
    Simplifiquei o código, declarando as variáveis assim que o usuário informa o valor pedido.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,23 +4,18 @@ import java.util.Locale;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
-        int Numero = 0;
-        String Agencia = "";
-        String NomeCliente = "";
-        double Saldo = 0.0;
-
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.ENGLISH);
         
         System.out.print("Número da agência: ");
-        Numero = scanner.nextInt();
+        int Numero = scanner.nextInt();
         scanner.nextLine(); // esta linha consome o \n do int
         System.out.print("Código da agência: ");
-        Agencia = scanner.nextLine();
+        String Agencia = scanner.nextLine();
         System.out.print("Nome do cliente: ");
-        NomeCliente = scanner.nextLine();
+        String NomeCliente = scanner.nextLine();
         System.out.print("Saldo do cliente: ");
-        Saldo = scanner.nextDouble();
+        double Saldo = scanner.nextDouble();
 
         System.out.println("Olá, " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + ", e seu saldo de " + Saldo + " já está disponível para saque.");
         scanner.close();
